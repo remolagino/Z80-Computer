11e82318b61a98f9a83ccd79d257c59e82f47c14
remolagino <remolagino@gmail;com>
2025-11-16 19:24:18 +0100

    Refactor keypad and I/O handling: update keypad decode matrix, enhance serial output functions, and clean up unused code in vdp_test.asm

--- asm/stdio.asm
+++ asm/stdio.asm
@@ -8,16 +8,26 @@ STREAM_OUT_PRINTER EQU 0x04
 STREAM_IN_SERIAL EQU 0x10
 STREAM_IN_KEYBOARD EQU 0x20
 
+BS EQU 0x08
+HTAB EQU 0x09
+LF EQU 0x0A
+CR EQU 0x0D
+CUR_UP EQU 0x11
+CUR_LEFT EQU 0x12
+CUR_DOWN EQU 0x13
+CUR_RIGHT EQU 0x14
+ESC EQU 0x1B
+
 ;VRAM_DATA EQU   0x40            ; PORT #0 - VRAM data port
     include "jumpTable.inc"
-    include "serial.asm"
+;    include "serial.asm"
     include "vdp_core.asm"
 
 
 PUTC: ; Output character in A to standard output at (HL) to standard output
     PUSH AF
     PUSH BC
-    LD B, A
+    LD B, A ; switch B and A as we work on A for stream_select comparison
     LD A, (STREAM_SELECT)
     AND STREAM_OUT_SERIAL
     CALL NZ, SIO_PUTC
@@ -56,47 +66,92 @@ GETC: ; Input in A from standard input
 .GetC_Done:
     RET
 
-SIO_PUTC:
+SIO_PUTC: ; char to print in B
     LD A, B
 ;    CALL HEX2STR
-    CP 0x08
+    CP BS
     JP Z, .backSpace
-    CP 0x0A
+    CP LF
     JP Z, .lineFeed
-    CP 0x0D
+    CP CR
     JP Z, .chariotReturn
-    CALL SendChar_A
+    CP CUR_UP
+    JP Z, .goUp
+    CP CUR_LEFT
+    JP Z, .goLeft
+    CP CUR_DOWN
+    JP Z, .goDown
+    CP CUR_RIGHT
+    JP Z, .goRight
+
+    CALL SENDCHAR_A
     RET
 .backSpace:
-    LD A, 0x08
-    CALL SendChar_A
+    LD A, BS
+    CALL SENDCHAR_A
     LD A, ' '
-    CALL SendChar_A
-    LD A, 0x08
-    CALL SendChar_A
+    CALL SENDCHAR_A
+    LD A, BS
+    CALL SENDCHAR_A
     RET
 .lineFeed:
-    CALL SendChar_A
-    LD A, '{'
     CALL SENDCHAR_A
     RET
 .chariotReturn:
-    CALL SendChar_A
-    LD A, '}'
+    CALL SENDCHAR_A
+    RET
+.goUp:
+    LD A, ESC
+    CALL SENDCHAR_A
+    LD A, '['
+    CALL SENDCHAR_A
+    LD A, 'A'
+    CALL SENDCHAR_A
+    RET
+.goLeft:
+    LD A, ESC
+    CALL SENDCHAR_A
+    LD A, '['
+    CALL SENDCHAR_A
+    LD A, 'D'
+    CALL SENDCHAR_A
+    RET
+.goDown:
+    LD A, ESC
+    CALL SENDCHAR_A
+    LD A, '['
+    CALL SENDCHAR_A
+    LD A, 'B'
+    CALL SENDCHAR_A
+    RET
+.goRight:
+    LD A, ESC
+    CALL SENDCHAR_A
+    LD A, '['
+    CALL SENDCHAR_A
+    LD A, 'C'
     CALL SENDCHAR_A
     RET
 
-VIDEO_PUTC:
+
+VIDEO_PUTC: ; char to print in B, at (HL)
     LD A, B
-    CP 0x08
+    CP BS
     JP Z, .backSpace
-    CP 0x09
+    CP HTAB
     JP Z, .horizontalTab
-    CP 0x0A
+    CP LF
     JP Z, .LineFeed
-    CP 0x0D
+    CP CR
     JP Z, .chariotReturn
-;    OUT (VRAM_DATA), A
+    CP CUR_UP
+    JP Z, .goUp
+    CP CUR_LEFT
+    JP Z, .goLeft
+    CP CUR_DOWN
+    JP Z, .goDown
+    CP CUR_RIGHT
+    JP Z, .goRight
     CALL WRITE_RAM
     INC HL
     RET
@@ -108,6 +163,8 @@ VIDEO_PUTC:
     POP AF
     RET
 .horizontalTab:
+    PUSH AF
+    PUSH BC
     LD A, L
     AND A, 0x07
     LD B, A
@@ -122,6 +179,8 @@ VIDEO_PUTC:
     OUT (VRAM_DATA), A
     INC HL
     DJNZ .tabLoop
+    POP BC
+    POP AF
     RET
 .LineFeed:
     PUSH BC
@@ -129,34 +188,136 @@ VIDEO_PUTC:
     ADD HL, BC
     POP BC
     RET
-.chariotReturn: ; ### SOME DEBUGGING TO DO
+.chariotReturn: ; Debugging done, missing PUSH POP
+    PUSH DE
     PUSH HL
+;    CALL Print_HL_Hex
     OR A ; clear carry
     LD DE, 80
 .numberOfRowsLoop:
     SBC HL, DE
+;    CALL Print_HL_Hex
     JP M, .beginningOfScreen ; if negative, we are at the beginning of the screen
     JP .numberOfRowsLoop
 .beginningOfScreen:
     LD DE, 80
     ADD HL, DE
+;    CALL Print_HL_Hex
     LD DE, HL
     POP HL
     OR A ; clear carry
+;    CALL Print_HL_Hex
     SBC HL, DE
+;    CALL Print_HL_Hex
+    POP DE
     RET
+.goLeft:
+    PUSH AF
+    PUSH BC
+    LD A, H
+    OR L
+    JP Z, .goLeft.exit ; if at left edge, do nothing
+    DEC HL
+.goLeft.exit:
+    POP BC
+    POP AF
+    RET
+.goRight:
+    PUSH AF
+    PUSH DE
+    PUSH HL
+    LD DE, HL ; check if possible to move right
+    LD HL, 80*26-1
+    OR A ; clear carry
+    SBC HL, DE
+    LD A, H
+    OR L
+    JP Z, .goRight.exit ; if at bottom right edge, do nothing
+    POP HL
+    INC HL
+    PUSH HL
+.goRight.exit:
+    POP HL
+    POP DE
+    POP AF
+    RET
+.goUp: ; # TODO UPDATE
+    PUSH AF
+    PUSH BC
+    PUSH DE
+    PUSH HL
+    LD DE, 80
+    OR A
+    SBC HL, DE
+    JP M, .goUp.exit ; if at top edge, do nothing
+    POP HL
+    LD DE, 80 ; move up one row
+    OR A ; clear carry
+    SBC HL, DE
+    PUSH HL
+.goUp.exit:
+    POP HL
+    POP DE
+    POP BC
+    POP AF
+    RET
+.goDown: ; # TODO - Update
+    PUSH AF
+    PUSH BC
+    PUSH DE
+    PUSH HL
+    LD DE, HL ; check if possible to move right
+    LD HL, 80*25-1 ; #TODO Parmetrize this at some point
+    OR A
+    SBC HL, DE
+    JP M, .goDown.exit ; if at left edge, do nothing
+    POP HL
+    LD DE, 80 ; move down one row
+    ADD HL, DE
+    PUSH HL
+.goDown.exit:
+    POP HL
+    POP DE
+    POP BC
+    POP AF
+    RET
+
+
+
+; Print_HL_Hex:
+;     PUSH AF
+;     LD A, "<"
+;     CALL SENDCHAR_A
+;     LD A, H
+;     CALL HEX2STR
+;     LD A, L
+;     CALL HEX2STR
+;     LD A, ">"
+;     CALL SENDCHAR_A
+;     POP AF
+;     RET
 
 PRINTER_PUTC:
     RET
 
 SIO_PUTS: ; #TODO# Problème sur l'adresse : ca tape dans le début du moniteur
     PUSH DE
-    EX DE, HL
-    CALL PrintString
-    CALL STRING_LENGTH
-    LD E, A
-    LD D, 0x00
-    ADD HL, DE
+    PUSH HL
+    LD HL, DE
+.loop:
+    LD A, (HL)
+    OR A
+    JP Z, .end
+    LD B, A
+    CALL SIO_PUTC ;
+    INC HL
+    JP .loop ; Continue printing the string
+.end:
+    LD B, CR
+    CALL SIO_PUTC ;
+    LD B, LF
+    CALL SIO_PUTC ;
+    POP HL
     POP DE
     RET
 
@@ -167,11 +328,16 @@ VIDEO_PUTS:
     LD A, (DE)
     CP 0x00
     JP Z, .exit
-    CALL PUTC
+    LD B, A
+    CALL VIDEO_PUTC
     INC DE
-    INC HL
+    ; INC HL
     JP .putsLoop   
 .exit:
+    LD B, CR
+    CALL VIDEO_PUTC ;
+    LD B, LF
+    CALL VIDEO_PUTC ;
     POP DE
     POP AF
     RET
--- asm/vdp_test.asm
+++ asm/vdp_test.asm
@@ -1,28 +1,17 @@
 ; V9938 Blue Screen Test Program for Z80
-RAM_START EQU 0x4000 ; Start of RAM
+; RAM_START EQU 0x4000 ; Start of RAM
 ; ======= Keypad Variables =======
 
-MODIFER_STATE EQU RAM_START + 0x0100 ; 1 byte
-MODIFERS EQU RAM_START + 0x0101 ; 1 byte
-KEYPAD_BUFFER EQU RAM_START + 0x0102 ; 4 characters buffer
-KEYPAD_STATE EQU RAM_START + 0x0110 ; Keypad state - ROWs * COLS bytes
+; MODIFER_STATE EQU RAM_START + 0x0100 ; 1 byte
+; MODIFERS EQU RAM_START + 0x0101 ; 1 byte
+; KEYPAD_BUFFER EQU RAM_START + 0x0102 ; 4 characters buffer
+; KEYPAD_STATE EQU RAM_START + 0x0110 ; Keypad state - ROWs * COLS bytes
 
 
-        ORG     0x5000          ; Start address
+    ORG     0x5000          ; Start address
 
-; ; V9938 Port definitions
-; VRAM_DATA EQU   0x40            ; PORT #0 - VRAM data port
-; VRAM_ADDR EQU   0x41            ; PORT #1 - VRAM Address 
-; VDP_STAT_REG EQU   0x41            ; PORT #1 - Status Register 
-; VDP_REG_SETUP EQU   0x41            ; PORT #1 - Register Setup
-
-; VDP_PAL_REG   EQU   0x42            ; PORT #2 - Palette Register port (MODE1)
-; VDP_REG_INDIR EQU   0x43            ; PORT #3 - Register Indirect Addressing 
-
-; COLOR_MAP_BASE_ADDR EQU 0x0A00 ; Base address of color table in VRAM
-; PATTERN_LAYOUT_TABLE_ADDR EQU 0x0000 ; Base address of pattern name table in VRAM
-  include "jumpTable.inc"
-
+    include "jumpTable.inc"
+    include "memoryMap.inc"
     JP START
 
     include "serial.asm"
@@ -38,61 +27,35 @@ START:
 
     LD HL, CR_LF
     CALL PRINT_STRING
-    LD HL, TestMsg
-    CALL PRINT_STRING
-    LD HL, CR_LF
-    CALL PRINT_STRING
-    LD HL, CR_LF
-    CALL PRINT_STRING
-
-
-;    LD D, 'X' ; character to write
-    CALL INIT_PATTERN_LAYOUT_TABLE
-    CALL INIT_COLOR_TABLE
 
     LD A, STREAM_OUT_VDP|STREAM_OUT_SERIAL
     LD (STREAM_SELECT), A
+    LD HL, StreamMsg
+    CALL PRINT_STRING
+    LD A, (STREAM_SELECT)
+    CALL HEX2STR
 
-    ; LD HL, 0x0060
-    ; CALL SET_VRAM_ADDR
+    LD HL, CR_LF
+    CALL PRINT_STRING
+    LD HL, CR_LF
+    CALL PRINT_STRING
+
+    CALL INIT_PATTERN_LAYOUT_TABLE
+    CALL INIT_COLOR_TABLE
 
     LD HL, 320
     LD DE, MeasureMsg
-; .stringLoop:
-;     LD A, (DE)
-;     OR A
-;     JP Z, .stringLoopEnd
-;     CALL PUTC
-;     INC DE
-;     JP .stringLoop
-; .stringLoopEnd:
     CALL PUTS
 
-    PUSH HL
-    LD HL, CR_LF
-    CALL PRINT_STRING
-    POP HL
-
-    LD HL, 400
     LD DE, TestMsg
-; .stringLoop1:
-;     LD A, (DE)
-;     OR A
-;     JP Z, .stringLoopEnd1
-;     CALL PUTC
-;     INC DE
-;     JP .stringLoop1
-; .stringLoopEnd1:
     CALL PUTS
 
     LD B, 0
-    LD HL, (CURSOR_IDX) ; put the cursor at 0
+    LD (CURSOR_IDX), HL ; put the cursor at 0
     LD C, 0x01
     CALL SET_BLINK
 
 .eventLoop:
-;    CALL DISPLAY_KEYPAD_STATE
-;    CALL DISPLAY_KEYPAD_BUFFER
     CALL Keypad_Scan2
     CALL ReceiveCharNB_A
 
@@ -101,24 +64,14 @@ START:
 
     CP '0'
     JP Z, .endLoop
-    CP '6'
-    JP Z, .right
-    CP '4'
-    JP Z, .left
-    CP '2'
-    JP Z, .down
-    CP '8'
-    JP Z, .up
     CP '9'
     JP Z, .screenVertical
     CP '3'
     JP Z, .screenHorizontal
     CP '5'
     JP Z, .rotateColor
-    CP 0x0D      ; Enter key
+    CP CR      ; Enter key
     JP Z, .enterKey
-    CP 0x08      ; Backspace key
-    JP Z, .backSpace
 
     LD HL, (CURSOR_IDX)
     LD C, 0x00
@@ -128,28 +81,14 @@ START:
     LD C, 0x01
     CALL SET_BLINK ; set blink at new position
     JP .eventLoop
-
 
 .enterKey:
     LD HL, (CURSOR_IDX)
     LD C, 0x00
     CALL SET_BLINK ; unset blink at current position
-    LD A, 0x0A
+    LD A, LF
     CALL PUTC
-    LD A, 0x0D
-    CALL PUTC
-    LD (CURSOR_IDX), HL
-    LD C, 0x01
-    CALL SET_BLINK ; set blink at new position
-    JP .eventLoop
-
-.backSpace:
-    LD C, 0x00
-    LD HL, (CURSOR_IDX)
-    CALL SET_BLINK ; unset blink at current position
-;    DEC HL
-;    LD (CURSOR_IDX), HL
-    LD A, 0x08
+    LD A, CR
     CALL PUTC
     LD (CURSOR_IDX), HL
     LD C, 0x01
@@ -166,18 +105,6 @@ START:
     AND 0x0F
     LD B, A
     JP .eventLoop
-.left:
-    CALL GO_LEFT
-    JP .eventLoop
-.right:
-    CALL GO_RIGHT
-    JP .eventLoop
-.up:
-    CALL GO_UP
-    JP .eventLoop
-.down:
-    CALL GO_DOWN
-    JP .eventLoop
 
 .screenVertical:
     LD      C, 18
@@ -190,8 +117,7 @@ START:
     OR B
 ;    CALL HEX2STR
     LD (SCREEN_OFFSET), A
-    CALL    WRITE_REG
-
+    CALL WRITE_REG
     JP .eventLoop
 .screenHorizontal:
     LD      C, 18
@@ -204,18 +130,37 @@ START:
     OR B
 ;    CALL HEX2STR
     LD (SCREEN_OFFSET), A
-    CALL    WRITE_REG
-
+    CALL WRITE_REG
     JP .eventLoop
-
 .endLoop:
     LD HL, CR_LF
     CALL PRINT_STRING
     RET                     ; or HALT
 
 
-SCREEN_OFFSET:
-    DB 0x00
+OnKeyPressed:
+    PUSH AF
+    PUSH BC
+    LD HL, (CURSOR_IDX)
+    LD C, 0x00
+    CALL SET_BLINK ; unset blink before moving
+    LD A, (IX)
+    CP CR
+    JP NZ, .notEnterKey
+    CALL PUTC
+    LD A, LF
+.notEnterKey:
+    CALL PUTC
+    LD (CURSOR_IDX), HL
+    LD C, 0x01
+    CALL SET_BLINK ; unset blink before moving
+.continue:
+    POP BC
+    POP AF
+    RET
+
+OnKeyReleased:
+    RET
 
 
 INIT_PATTERN_LAYOUT_TABLE: ; write at adress 0x0000
@@ -225,7 +170,6 @@ INIT_PATTERN_LAYOUT_TABLE: ; write at adress 0x0000
 
     LD HL, PATTERN_LAYOUT_TABLE_BASE_ADDR
     CALL SET_VRAM_ADDR
-
     LD HL, 80*27 ; number of bytes to write (0x0B0D = 2821 bytes)
 .loop:
     LD A, ' ' ; value to write to initialize VRAM
@@ -273,154 +217,22 @@ INIT_COLOR_TABLE: ; write at adress 0x0000
     POP AF
     RET
 
-    
-OnKeyPressed:
-    PUSH AF
-    PUSH BC
-    PUSH DE
-    PUSH HL
-
-    LD HL, PressedMsg
-    CALL PRINT_STRING
-    CALL SendChar_IX
-    LD HL, CR_LF
-    CALL PRINT_STRING
-
-    LD A, (IX)
-    CP '4'
-    CALL Z, GO_LEFT
-    CP '6'
-    CALL Z, GO_RIGHT
-    CP '8'
-    CALL Z, GO_UP
-    CP '2'
-    CALL Z, GO_DOWN
-.continue:
-    POP HL
-    POP DE
-    POP BC
-    POP AF
-    RET
-
-GO_LEFT:
-    PUSH AF
-    PUSH BC
-    PUSH HL
-    LD HL, (CURSOR_IDX) ; check if possible to move left
-    LD A, H
-    OR L
-    JP Z, .leftExit ; if at left edge, do nothing
-
-    LD C, 0x00
-    CALL SET_BLINK ; unset blink before moving
-    LD HL, (CURSOR_IDX)
-    DEC HL
-    LD (CURSOR_IDX), HL
-
-    LD C, 0x01
-    CALL SET_BLINK ; set blink at new position
-.leftExit:
-    POP HL
-    POP BC
-    POP AF
-    RET
-    
-GO_RIGHT:
-    PUSH DE
-    PUSH HL
-    LD DE, (CURSOR_IDX) ; check if possible to move right
-    LD HL, 80*26-1
-    OR A
-    SBC HL, DE
-    LD A, H
-    OR L
-    JP Z, .rightExit ; if at bottom right edge, do nothing
-    LD HL, (CURSOR_IDX)
-    LD C, 0x00
-    CALL SET_BLINK ; unset blink before moving
-    LD HL, (CURSOR_IDX)
-    INC HL
-    LD (CURSOR_IDX), HL
-    LD C, 0x01
-    CALL SET_BLINK ; set blink at new position
-.rightExit:
-    POP HL
-    POP DE
-    RET
-
-GO_UP:
-    PUSH AF
-    PUSH BC
-    PUSH DE
-    PUSH HL
-    LD HL, (CURSOR_IDX) ; check if possible to move up
-    LD DE, 80
-    OR A
-    SBC HL, DE
-    JP M, .upExit ; if at top edge, do nothing
-    LD HL, (CURSOR_IDX)
-    LD C, 0x00
-    CALL SET_BLINK ; unset blink before moving
-    LD HL, (CURSOR_IDX)
-    LD DE, 80 ; move up one row
-    OR A ; clear carry
-    SBC HL, DE
-    LD (CURSOR_IDX), HL
-    LD C, 0x01
-    CALL SET_BLINK ; set blink at new position
-.upExit:
-    POP HL
-    POP DE
-    POP BC
-    POP AF
-    RET
-
-GO_DOWN:
-    PUSH AF
-    PUSH BC
-    PUSH DE
-    PUSH HL
-    LD DE, (CURSOR_IDX) ; check if possible to move right
-    LD HL, 80*25-1
-    OR A
-    SBC HL, DE
-    JP M, .downExit ; if at left edge, do nothing
-    LD HL, (CURSOR_IDX)
-    LD C, 0x00
-    CALL SET_BLINK ; unset blink before moving
-    LD HL, (CURSOR_IDX)
-    LD DE, 80 ; move down one row
-    ADD HL, DE
-    LD (CURSOR_IDX), HL
-    LD C, 0x01
-    CALL SET_BLINK ; set blink at new position
-.downExit:
-    POP HL
-    POP DE
-    POP BC
-    POP AF
-    RET
-
-OnKeyReleased:
-    PUSH HL
-    LD HL, ReleasedMsg
-    CALL PRINT_STRING
-    CALL SendChar_IX
-    LD HL, CR_LF
-    CALL PRINT_STRING
-    POP HL
-    RET
 
 CR_LF:
-    DB 0x0A, 0x0D, 0x00 ; Carriage return + line feed
+    DB CR, LF, 0x00 ; Carriage return + line feed
 
 PressedMsg:
     DB 'Key Pressed : ', 0x00
 ReleasedMsg:
     DB 'Key Released : ', 0x00
+StreamMsg:
+    DB "Stream Selected : ", 0x00
 TestMsg:
-    DB "This.", 0x09,"is.a.te", 0x08, "st mess", 0x09, "age.!!", 0x0D,  "Prout prout !", 0x00,0x0A
+    DB HTAB, HTAB, "This.is.a.test message.!!", CR, LF,  "Prout", CUR_UP, CUR_LEFT, CUR_LEFT, "prout !", 0x00
 MeasureMsg:
     DB "01234567012345670123456701234567012345670123456701234567", 0x00
+
+SCREEN_OFFSET:
+    DB 0x00
 
     END
